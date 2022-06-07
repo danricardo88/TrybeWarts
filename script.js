913c91fa46864d62301a41a13454a9a6cdb77c50
DannOut <danielxdn@gmail.com>
2022-06-07 17:30:05 -0300

    Exercicios feitos até o 4

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const buttonLogin = document.querySelector('#button-login');
 function returnLogin() {
   const email = emailLogin.value;
   const passVali = passwordLogin.value;
+  console.log(email);
+  console.log(passVali);
 
-  if ((email === 'trybe@teste.com') && (passVali === '123456')) {
+  if (email === 'tryber@teste.com' && passVali === '123456') {
     alert('Olá, Tryber!');
   } else alert('Email ou senha inválidos.');
 }

575300c9ea6652119a4f86c5e43454656b13a525
DannOut <danielxdn@gmail.com>
2022-06-08 14:09:52 -0300

    Finalizando projeto Trybewarts, falta estilizar

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const buttonLogin = document.querySelector('#button-login');
 const idAgreement = document.querySelector('#agreement');
 const textArea = document.querySelector('#textarea');
 const submitBtn = document.querySelector('#submit-btn');
+const evaluationForm = document.querySelector('#evaluation-form');
 
 // validation
 function returnLogin() {
@@ -39,9 +40,81 @@ const countText = () => {
 textArea.addEventListener('keyup', countText);
 
 // submit Q21
-const cleanText = document.querySelector('#hide-all');
+const formData = document.querySelector('#form-data');
 
+// Function Get-name (Name & surname);
+function gettingName() {
+  const inputName = document.querySelector('#input-name');
+  const inputLastname = document.querySelector('#input-lastname');
+  const getName = document.querySelector('#get-name');
+  getName.innerHTML = `${inputName.value} ${inputLastname.value}`;
+}
+// function get-email
+function gettingMail() {
+  const inputEmail = document.querySelector('#input-email');
+  const getMail = document.querySelector('#get-email');
+  getMail.innerHTML = inputEmail.value;
+}
+// function selectHouse
+function selecHouses() {
+  const inputHouse = document.querySelector('#get-house');
+  const getHouse = document.querySelector('#house');
+  inputHouse.innerHTML = getHouse.value;
+}
+
+// function getFamily
+function selectFamily() {
+  const inputFamily = document.getElementsByName('family');
+  const getFamily = document.querySelector('#get-family');
+  for (let i = 0; i < inputFamily.length; i += 1) {
+    if (inputFamily[i].checked) {
+      getFamily.innerHTML = inputFamily[i].value;
+    } else;
+  }
+}
+
+// function selectSubject
+function selectSubject() {
+  const inputSubject = document.getElementsByName('HoFs');
+  const getSubject = document.querySelector('#get-subject');
+  const allSubjects = [];
+  for (let i = 0; i < inputSubject.length; i += 1) {
+    if (inputSubject[i].checked) {
+      allSubjects.push(` ${inputSubject[i].value}`);
+    } else;
+  }
+  getSubject.innerHTML = allSubjects;
+}
+
+// function selectEvaluation
+function selectEvaluation() {
+  const inputRate = document.getElementsByName('rate');
+  const getEvaluation = document.querySelector('#get-evaluation');
+  for (let i = 0; i < inputRate.length; i += 1) {
+    if (inputRate[i].checked) {
+      getEvaluation.innerHTML = inputRate[i].value;
+    } else;
+  }
+}
+
+// function textAreaInformation
+function textAreaInformation() {
+  const inputTextArea = document.querySelector('#textarea');
+  const getTextArea = document.querySelector('#get-textarea');
+  getTextArea.innerHTML = inputTextArea.value;
+}
+/*
+<strong>Observações: </strong><span id="get-textarea"></span> */
 submitBtn.addEventListener('click', (event) => {
-  cleanText.className = 'hideAll';
+  evaluationForm.style.display = 'none';
+  formData.style.display = 'flex';
+  formData.style.flexFlow = 'column nowrap';
   event.preventDefault();
+  gettingName();
+  gettingMail();
+  selecHouses();
+  selectFamily();
+  selectSubject();
+  selectEvaluation();
+  textAreaInformation();
 });
